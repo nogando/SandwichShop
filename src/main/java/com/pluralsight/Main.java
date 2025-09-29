f51146b1ebfb8dbdde9dc3720d3b3545a7747793
nogando <nathanielogando@gmail.com>
2025-09-29 11:17:33 -0400

    project one

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,17 +1,49 @@
 package com.pluralsight;
 
+import jdk.dynalink.beans.StaticClass;
+
+import java.awt.*;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner scanner = new Scanner(System.in);
+
+        double price = 0;
+        int age;
+        double discountPrice = 0;
+        double discount;
+
+        System.out.println("Please enter the size of the sandwich 1 or 2");
+        int sandwichNum = scanner.nextInt();
+
+        if (sandwichNum == 1){
+
+            price = 5.45;
+
         }
+        else if (sandwichNum == 2){
+
+            price = 8.95;
+        }
+        System.out.println("How old are you?");
+        age = scanner.nextInt();
+            if (age <= 17){
+                discount = price * .10;
+                discountPrice = price - discount;
+            }
+            else if (age >= 65){
+
+                discount = price * .20;
+                discountPrice = price - discount;
+
+            }
+
+
+
+        System.out.printf("The price of your sandwich is: %.2f $ \n",discountPrice);
     }
 }
